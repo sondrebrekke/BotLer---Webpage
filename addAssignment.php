e3c0fcd3b3d83377cc5dd5903403965df014e384
Sondre Brekke <sondrbre@stud.ntnu.no>
2017-04-06 10:41:03 +0200

    Fixed links and connections between pages

--- addAssignment.php
+++ addAssignment.php
@@ -54,12 +54,12 @@
             <div class="navbar-collapse collapse">
             <br><img src="/marentno/BotLer/assets/img/Logo(1).png" alt="" width="20%" height="20%"> </a>
                 <ul class="nav navbar-nav navbar-right">
-                <li><a href="http://folk.ntnu.no/sondrbre/homepage.php"> <br>Home<br><br></a></li>
-                <li><a href="http://folk.ntnu.no/sondrbre/changePassword.php"> <br>Change Password<br><br></a></li>
-                <li><a href="http://folk.ntnu.no/sondrbre/viewAssignment.php"> <br>View Assignments<br><br></a></li>
-                <li><a href="http://folk.ntnu.no/sondrbre/addAssignment.php"> <br>Add Assignment<br><br></a></li>
-                <li><a href="http://folk.ntnu.no/sondrbre/viewFeedback.php"> <br>View Feedback<br><br></a></li>
-                <li><a href="http://folk.ntnu.no/sondrbre/logout.php"> <br>Log Out<br><br></a></li>
+                <li><a href="homepage.php" id = "home"> <br>Home<br><br></a></li>
+                <li><a href="changePassword.php" id = "changePassword"> <br>Change Password<br><br></a></li>
+                <li><a href="viewAssignment.php" id = "viewAssignment"> <br>View Assignments<br><br></a></li>
+                <li><a href="addAssignment.php" id = "addAssignment"> <br>Add Assignment<br><br></a></li>
+                <li><a href="viewFeedback.php" id = "viewFeedback"> <br>View Feedback<br><br></a></li>
+                <li><a href="logout.php" id = "logOut"> <br>Log Out<br><br></a></li>
                 <li><a><center><?php echo "Welcome, <br>$name!";?><br><br></center></a></li>
 
 
@@ -79,7 +79,7 @@
     <div class="container">
         <div class="row main-low-margin text-center">
             <div class="col-md-5 col-sm-5">
-                <img src="/marthaan/BotLer/assets/img/Mashup1.png" alt="" width="30%" height="30%"/>
+                <img src="/marthaan/BotLer/assets/img/botler1.png" alt="" width="50%" height="50%"/>
                 <p>
                     <br>
                     By adding assignments to your course, both mandatory and preparatory, your students recieve information directly into their BotLer application.
@@ -89,23 +89,23 @@
             <div class="col-md-7 col-sm-7">
                 <h3>Add Assignment</h3>
                 <hr>
-                <form action="addedAssignments.php" method="post">
+                <form action="addedAssignments.php" method="post" id = "assignmentForm">
                     <div class="row">
                         <div class="col-md-6 col-sm-6">
                             <div class="form-group">
-                                <input type="text" class="form-control" required="required" name = "name" placeholder="Name" pattern="^[a-zA-Z0-9]+$" >
+                                <input type="text" class="form-control" required="required" name = "name" placeholder="Name">
                             </div>
                         </div>
                         <div class="col-md-6 col-sm-6">
                             <div class="form-group">
-                                <input type="text" class="form-control" required="required"  name = "deadline" placeholder="Deadline:  YYYY-MM-DD" pattern="^[a-zA-Z0-9]+$" >
+                                <input type="text" class="form-control" required="required"  name = "deadline" placeholder="Deadline:  YYYY-MM-DD">
                             </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-md-12 col-sm-12">
                             <div class="form-group">
-                                <textarea id="Textarea1" required="required" class="form-control" rows="3" name = "description" placeholder="Description" pattern="^[a-zA-Z0-9]+$"></textarea>
+                                <textarea id="Textarea1" required="required" class="form-control" rows="3" name = "description" placeholder="Description"></textarea>
                             </div>
                             <div class="form-group">
                                 <button type="submit" class="btn btn-primary">Add Assignment</button>
--- viewAssignment.php
+++ viewAssignment.php
@@ -72,12 +72,12 @@
             <div class="navbar-collapse collapse">
             <br><img src="/marentno/BotLer/assets/img/Logo(1).png" alt="" width="20%" height="20%"> </a>
                 <ul class="nav navbar-nav navbar-right">
-                <li><a href="http://folk.ntnu.no/sondrbre/homepage.php"> <br>Home<br><br></a></li>
-                <li><a href="http://folk.ntnu.no/sondrbre/changePassword.php"> <br>Change Password<br><br></a></li>
-                <li><a href="http://folk.ntnu.no/sondrbre/viewAssignment.php"> <br>View Assignments<br><br></a></li>
-                <li><a href="http://folk.ntnu.no/sondrbre/addAssignment.php"> <br>Add Assignment<br><br></a></li>
-                <li><a href="http://folk.ntnu.no/sondrbre/viewFeedback.php"> <br>View Feedback<br><br></a></li>
-                <li><a href="http://folk.ntnu.no/sondrbre/logout.php"> <br>Log Out<br><br></a></li>
+                <li><a href="homepage.php" id = "home"> <br>Home<br><br></a></li>
+                <li><a href="changePassword.php" id = "changePassword"> <br>Change Password<br><br></a></li>
+                <li><a href="viewAssignment.php" id = "viewAssignment"> <br>View Assignments<br><br></a></li>
+                <li><a href="addAssignment.php" id = "addAssignment"> <br>Add Assignment<br><br></a></li>
+                <li><a href="viewFeedback.php" id = "viewFeedback"> <br>View Feedback<br><br></a></li>
+                <li><a href="logout.php" id = "logOut"> <br>Log Out<br><br></a></li>
                 <li><a><center><?php echo "Welcome, <br>$name!";?><br><br></center></a></li>
 
 
@@ -97,7 +97,7 @@
     <br>
 <div class="container" align="center">
         
-              <h2>Assigments for <?php echo $subject_code ?></h2>            
+              <h2>Assignments for <?php echo $subject_code ?></h2>            
               <table class="table table-striped">
                 <thead>
                   <tr>
